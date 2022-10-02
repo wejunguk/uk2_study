46f1be5463d0762741816d6e43efdf49bdccbb7b
wejugnuk <uk20915@naver.com>
2022-10-02 16:39:06 +0900

    js

--- javaScript/README.md
+++ javaScript/README.md
@@ -3,6 +3,18 @@
 - 코딩실수나 에러를 포착할 수 있다 -> 디버깅 가능
 - 자바스크립트가 더 빨리 실행할 수 있게 된다.
 
+# 객체 데이터
+- let obj = { abc: 123 }; //console.log(obj.abc); //123
 
-<br />
+# 변수
+ const : 재할당 불가능, let : 재할당 가능
 
+# DOM API (Document Object Model Application Programming Interface)
+- DOM : HTML의 여러가지 모델, div요소,span요소, input요소 ..
+- API : 웹 사이트가 동작하기위해 입력하는 프로그래밍 '명령'
+- 자바스크립트로 HTML을 제어할때 사용하는 명령
+
+# 메소드 체이닝
+- split : 문자를 인수 기준으로 쪼개서 배열로 변환
+- reverse : 배열을 뒤집기
+- join : 배열을 인수 기준으로 문자로 병합해 반환
---
+++ javaScript/app/src/main/java/uk2/test/main.js
@@ -0,0 +1,55 @@
+// let myName = "KH";
+// let hello = `Hello ${myName}`;
+
+// console.log(myName);
+// console.log(hello);
+
+// 객체 데이터
+// let user = {
+//   name: 'we',
+//   age: 20,
+//   isValid: true
+// };
+
+// console.log(user.name);
+// console.log(user.age);
+// console.log(user.isValid);
+
+// 배열 데이터
+// let fruits = ['apple', 'banana', 'cherry'];
+
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[2]);
+
+// DOM API - querySelector
+// let boxEl = document.querySelector('.box');
+
+// console.log(boxEl);
+
+// boxEl.addEventListener('click', function(){
+//   console.log('Click!');
+//   boxEl.classList.add('active');
+//   console.log(
+//     boxEl.classList.contains('active')
+//   );
+//   boxEl.classList.remove('active');
+//   console.log(
+//     boxEl.classList.contains('active')
+//   );
+// });
+
+// DOM API - querySelectorAll
+// const boxEls = document.querySelectorAll('.box');
+
+// boxEls.forEach(function (boxEl, index) {
+//   boxEl.classList.add(`order-${index + 1}`);
+//   console.log(index, boxEl);
+// });
+
+const boxEl = document.querySelector('.box');
+
+console.log(boxEl.textContent);
+
+boxEl.textContent = 'Hi';
+console.log(boxEl.textContent);
